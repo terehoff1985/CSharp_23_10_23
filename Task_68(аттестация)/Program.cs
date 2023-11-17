60de813d307647768acb74aa5a89d506d4d355bd
Terehoff <Artem>
2023-11-17 09:15:00 +0700

    Рещены задачи семинара дз для аттестации

---
+++ Task_68(аттестация)/Program.cs
@@ -0,0 +1,26 @@
+﻿ /* Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+ Даны два неотрицательных числа m и n.
+ m = 2, n = 3 -> A(m,n) = 9
+ m = 3, n = 2 -> A(m,n) = 29  */
+
+ Console.Clear();
+
+
+int AckermannFunction(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return AckermannFunction(m - 1, 1);
+    }
+    else
+    {
+        return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+    } 
+}
+int result = AckermannFunction(2,3);
+System.Console.WriteLine(result);
+
